b876aa616427ca5ef7f066b6ef02e147f0c0c002
_ stefthedoggo <>
2021-04-15 15:14:03 +0000

    Work on Eval Command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,8 +1,18 @@
-import discord, os, aiohttp
+import discord, os, aiohttp, sys, contextlib, io
 from subprocess import run
 from discord.ext import commands
+from io import StringIO
 
 print('Loading in...')
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+	old = sys.stdout
+	if stdout is None:
+		stdout = StringIO()
+	sys.stdout = stdout
+	yield stdout
+	sys.stdout = old
 
 class Admin(commands.Cog):
 	def __init__(self, bot):
@@ -12,15 +22,41 @@ class Admin(commands.Cog):
 	@commands.is_owner()
 	async def console(self, ctx, *args):
 		item = os.popen(' '.join(args)).readlines()
-		output = eval(str(item)[1:-1])
+		output = '\n'.join(eval(str(item)))
 		try:
-			await ctx.reply(output, mention_author=False)
-		except Exception as e:
+			await ctx.channel.send(output)
+		except:
 			session = aiohttp.ClientSession()
-			async with session.post("https://hst.sh/documents", data="your content") as resp:
+			async with session.post("https://hst.sh/documents", data=output) as resp:
 				body = await resp.json()
-				print("https://hst.sh/" + body.key)
-			print('large! ' + e)
+				output = "https://hst.sh/" + body['key']
+			await session.close()
+			await ctx.reply(str(output), mention_author=False)
+	
+	@commands.command(name='eval', aliases=['evaluate', 'ev'], pass_context=True, hidden=True)
+	@commands.is_owner()
+	async def eval(self, ctx, *args):
+		str_obj = io.StringIO()
+		try:
+			with contextlib.redirect_stdout(str_obj):
+				exec(' '.join(args))
+			output = str_obj.getvalue()
+			
+			try:
+				await ctx.reply(str(output), mention_author=False)
+
+			except:
+				session = aiohttp.ClientSession()
+				async with session.post("https://hst.sh/documents", data=output) as resp:
+					body = await resp.json()
+					print(body)
+					output = "https://hst.sh/" + body['key']
+				await session.close()
+				await ctx.reply(str(output), mention_author=False)
+		except Exception as e:
+			await ctx.reply(f'<:error:830635116048810005> Error while Executing script: \n```py\n{" ".join(args)}\n```\n**Error:** ```py\n{e}\n```', mention_author=False)
+
+		
 
 
 def setup(bot):
