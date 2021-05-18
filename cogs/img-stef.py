cb7cdb77368abb159b09266195723e0be60e12d1
stef-the <75397677+stef-the@users.noreply.github.com>
2021-05-18 10:28:13 -0700

    E

--- cogs/img-stef.py
+++ cogs/img-stef.py
@@ -1,12 +1,17 @@
 import discord, json, os
 from discord.ext import commands
+from PIL import Image
 
 print('Loading in...')
 
-class ticket_cog(commands.Cog, name='Tickets'):
+def imgto(image: dict=None, out_type:str='png'):
+	if image['name'].endswith('.png'):
+		return(False)
+
+class testing(commands.Cog, name='Tickets'):
 	def __init__(self, bot):
 		self.bot = bot
 
 def setup(bot):
-	bot.add_cog(ticket_cog(bot))
-	print('cogs.ticket - injected')
+	bot.add_cog(testing(bot))
+	print('cogs.img-stef - injected')
